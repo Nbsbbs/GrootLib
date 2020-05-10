27850e34e1e9d9fbbff71da0b8572c38833ec97b
Mikhail Solovev <m.soloviov@k50dev.ru>
2020-05-10 17:46:17 +0300

    user types

--- src/Entity/User/UserSourceType.php
+++ src/Entity/User/UserSourceType.php
@@ -6,18 +6,22 @@ namespace Noobus\GrootLib\Entity\User;
 
 class UserSourceType
 {
-    public const TYPE_UNKNOWN = 'unknown';
-    public const TYPE_SEARCH = 'se';
-    public const TYPE_FEED = 'feed';
-    public const TYPE_BOOKMARK = 'bookmark';
-    public const TYPE_BOT = 'bot';
+    public const TYPE_UNKNOWN = 'unknown'; // from unknown site
+    public const TYPE_SEARCH = 'search'; // from search engines
+    public const TYPE_BOOKMARK = 'bookmark'; // must have cookie and no referer
+    public const TYPE_NOREF = 'noref'; // no referer, no cookie
+    public const TYPE_FEED = 'feed'; // no referer, no cookie
+    public const TYPE_TRADE = 'trade'; // from a list of trade domains
+
+    public const INTERNAL = 'internal'; // from our site, but no session
 
     public const VALID_TYPES = [
         self::TYPE_UNKNOWN,
         self::TYPE_SEARCH,
         self::TYPE_FEED,
         self::TYPE_BOOKMARK,
-        self::TYPE_BOT,
+        self::TYPE_TRADE,
+        self::TYPE_NOREF,
     ];
 
     /**
